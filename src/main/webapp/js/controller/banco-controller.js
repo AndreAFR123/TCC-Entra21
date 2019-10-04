69a23b65efaab2a59c8d92a11fd72d8f3007389b
danielranghetti <danielranghetti48@gmail.com>
2019-10-03 21:43:55 -0300

    contas

--- src/main/webapp/js/controller/banco-controller.js
+++ src/main/webapp/js/controller/banco-controller.js
@@ -44,5 +44,58 @@ banSys.controller("bancoController", function($scope, $http, $routeParams) {
             console.log(response.status);
         });
     };
+    $scope.transferenciaConta = function() {
+		$http({
+			method : 'PUT',
+			url : urlApi + 'contas/'+ id + '/' + saldo_conta + '/' + saldo + '/' +  num + '/'
+		}).then(function(response) {
+			$scope.listaConta.push(response.data);
+			
+		}, function(response) {
+			console.log('error da transferencia');
+			console.log(response.data);
+			console.log(response.status);
+		});
+	};
+	
+	$scope.emprestimoConta = function() {
+		$http({
+			method : 'PUT',
+			url : urlApi + 'contas/' + id + '/' + saldo_conta + '/' + emprestimo_conta + '/'
+		}).then(function(response) {
+			$scope.listaConta.push(response.data);
+		}, function(response) {
+			console.log('error do emprestimo');
+			console.log(response.data);
+			console.log(response.status);
+		});
+	};
+	$scope.saqueConta = function() {
+		$http({
+			method : 'PUT',
+			url : urlApi + 'contas/saque/' + id + '/' + saldo_conta + '/'
+		}).then(function(response) {
+			$scope.listaConta.push(response.data);
+			$scope.listaConta();
+		}, function(response) {
+			console.log('error do saque');
+			console.log(response.data);
+			console.log(response.status);
+		});
+	};
+	
+	$scope.depositoConta = function() {
+		$http({
+			method : 'PUT',
+			url : urlApi + 'contas/deposito/' + id + '/' + saldo_conta + '/'
+		}).then(function(response) {
+			$scope.listaConta = response.data;
+		}, function(response) {
+			console.log('error do deposito');
+			console.log(response.data);
+			console.log(response.status);
+		});
+	};
+});
 
 });
